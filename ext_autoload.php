ddf3d3baece9c181c6695a12146bff5e66e023e7
Sebastian Schmidt <ss@braune-digital.com>
2014-07-20 11:41:22 +0200

    Added tx_euldap_div to autoloader and build initial import job

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,6 +1,7 @@
 <?php
 
 return array(
+    'tx_euldap_div' => t3lib_extMgm::extPath('eu_ldap', 'mod1/class.tx_euldap_div.php'),
 	'tx_euldap_import' => t3lib_extMgm::extPath('eu_ldap', 'scheduler/class.tx_euldap_import.php'),
 	'tx_euldap_import_addfields' => t3lib_extMgm::extPath('eu_ldap', 'scheduler/class.tx_euldap_import_addFields.php'),
 );
--- scheduler/class.tx_euldap_import.php
+++ scheduler/class.tx_euldap_import.php
@@ -1,9 +1,34 @@
 <?php
 
 class tx_euldap_import extends tx_scheduler_Task {
-	
-	public function execute() {
-		
+
+    /**
+     * Execute the import task and import all be_users for all servers with be_authentication
+     *
+     * @return bool
+     */
+    public function execute() {
+		$eu_ldap =  t3lib_div::makeInstance('tx_euldap_div');
+
+        // Grab all ldap servers
+        $dbres = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
+            '*',
+            'tx_euldap_server','authenticate_be = 1'
+        );
+        while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($dbres)) {
+            $arrServers[] = $row;
+        }
+
+        try {
+            //Iterate and import ignoring the groups for now
+            foreach($arrServers as $currentServer) {
+                $eu_ldap->import_users($currentServer, array(), 'be_users');
+            }
+        } catch (Exception $e) {
+            return false;
+        }
+            
+        return true;
 	}
 	
 	public function getAdditionalInformation() {
